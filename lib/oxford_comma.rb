823c8b84613d491482efc713b847988724960846
Sarah H <sarahrose.henderson@gmail.com>
2019-09-22 17:38:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
-array.join(" and ")
+if array.length == 2
+  return "#{array[0]} and #{array[1]}"
+elsif 2 < array.length
+  array[-1].insert(0, "and")
+end
+array.join(",")
 
 end
